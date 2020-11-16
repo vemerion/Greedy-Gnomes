8836cbbd7bdfb749e97c33c29e7bbb54999fbfa0
vemerion <vemerion@gmail.com>
2020-11-16 22:18:06 +0100

    Add collect quest item goal to gnome

--- src/main/java/mod/vemerion/greedygnomes/entity/GreedyGnomeEntity.java
+++ src/main/java/mod/vemerion/greedygnomes/entity/GreedyGnomeEntity.java
@@ -1,7 +1,16 @@
 package mod.vemerion.greedygnomes.entity;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.collect.ImmutableList;
+
 import mod.vemerion.greedygnomes.ModInit;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.ai.brain.task.LookTargetUtil;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
@@ -16,11 +25,17 @@ import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.world.World;
 
 public class GreedyGnomeEntity extends PathAwareEntity {
+
+	private static final List<Quest> POSSIBLE_QUESTS = ImmutableList.of(new Quest(Items.APPLE, 20, 30),
+			new Quest(Items.SNOWBALL, 20, 30), new Quest(Items.BIRCH_LOG, 50, 64),
+			new Quest(Items.DARK_OAK_LOG, 50, 64), new Quest(Items.COAL, 50, 64), new Quest(Items.IRON_PICKAXE, 1, 1),
+			new Quest(ModInit.GREEDY_GNOME_BUNDLE_ITEM, 1, 1));
 
 	private static final TrackedData<ItemStack> QUEST = DataTracker.registerData(GreedyGnomeEntity.class,
 			TrackedDataHandlerRegistry.ITEM_STACK);
@@ -41,7 +56,8 @@ public class GreedyGnomeEntity extends PathAwareEntity {
 	@Override
 	protected void initGoals() {
 		goalSelector.add(1, new SwimGoal(this));
-		goalSelector.add(2, new MeleeAttackGoal(this, 2.0D, true));
+		goalSelector.add(2, new CollectQuestItemGoal(this));
+		goalSelector.add(3, new MeleeAttackGoal(this, 2.0D, true));
 		goalSelector.add(5, new WanderAroundFarGoal(this, 1));
 		goalSelector.add(11, new LookAtEntityGoal(this, PlayerEntity.class, 10.0F));
 		goalSelector.add(7, new LookAroundGoal(this));
@@ -49,6 +65,7 @@ public class GreedyGnomeEntity extends PathAwareEntity {
 
 	}
 
+	// TODO: Sync quest properly to client
 	@Override
 	protected void initDataTracker() {
 		super.initDataTracker();
@@ -59,4 +76,110 @@ public class GreedyGnomeEntity extends PathAwareEntity {
 		return dataTracker.get(QUEST);
 	}
 
+	private void setQuest(ItemStack questStack) {
+		dataTracker.set(QUEST, questStack);
+	}
+
+	private void changeQuest() {
+		setQuest(randomQuest());
+	}
+
+	private ItemStack randomQuest() {
+		return POSSIBLE_QUESTS.get(getRandom().nextInt(POSSIBLE_QUESTS.size())).createQuestInstance(getRandom());
+	}
+
+	private static class CollectQuestItemGoal extends Goal {
+
+		private GreedyGnomeEntity gnome;
+		private ItemEntity target;
+		private int collectingTimer;
+
+		public CollectQuestItemGoal(GreedyGnomeEntity gnome) {
+			this.gnome = gnome;
+			this.setControls(EnumSet.of(Goal.Control.MOVE));
+		}
+
+		@Override
+		public boolean canStart() {
+			return !getNearbyQuestItems().isEmpty();
+		}
+
+		@Override
+		public void start() {
+			List<ItemEntity> questItems = getNearbyQuestItems();
+			if (!questItems.isEmpty()) {
+				gnome.getNavigation().startMovingTo(questItems.get(0), (double) 1.2F);
+			}
+		}
+
+		@Override
+		public void stop() {
+		}
+
+		@Override
+		public void tick() {
+			List<ItemEntity> nearbyQuestItems = getNearbyQuestItems();
+			if (!nearbyQuestItems.isEmpty()) {
+				ItemEntity nearbyQuestItem = nearbyQuestItems.get(0);
+				gnome.getNavigation().startMovingTo(nearbyQuestItems.get(0), (double) 1.2F);
+				if (nearbyQuestItem.squaredDistanceTo(gnome) < 4) {
+					if (nearbyQuestItem != target) {
+						collectingTimer = 40;
+					} else {
+						if (collectingTimer-- < 0) {
+							if (target.getStack().getItem() == Items.GOLD_INGOT) {
+								target.getStack().decrement(1);
+								gnome.changeQuest();
+							} else {
+								ItemStack questStack = gnome.getQuest();
+								Item questItem = questStack.getItem();
+								int decrement = Math.min(questStack.getCount(), target.getStack().getCount());
+								System.out.println(decrement);
+								questStack.decrement(decrement);
+								target.getStack().decrement(decrement);
+
+								// Quest completed
+								if (questStack.isEmpty()) {
+									if (questItem == ModInit.GREEDY_GNOME_BUNDLE_ITEM) {
+										GreedyGnomeEntity fromBundle = new GreedyGnomeEntity(gnome.world);
+										fromBundle.updatePosition(gnome.getX(), gnome.getY(), gnome.getZ());
+										gnome.world.spawnEntity(fromBundle);
+									} else {
+										LookTargetUtil.give(gnome, new ItemStack(Items.DIAMOND),
+												gnome.getPos().add(0.0D, 1.0D, 0.0D));
+									}
+									gnome.changeQuest();
+								}
+							}
+							target = null;
+						}
+					}
+					target = nearbyQuestItem;
+				}
+			}
+		}
+
+		private List<ItemEntity> getNearbyQuestItems() {
+			return gnome.world.getEntitiesByClass(ItemEntity.class, gnome.getBoundingBox().expand(8.0D, 8.0D, 8.0D),
+					e -> gnome.getQuest().getItem() == e.getStack().getItem()
+							|| e.getStack().getItem() == Items.GOLD_INGOT);
+		}
+
+	}
+
+	private static class Quest {
+		private Item item;
+		private int min;
+		private int max;
+
+		private Quest(Item item, int min, int max) {
+			this.item = item;
+			this.min = min;
+			this.max = max;
+		}
+
+		private ItemStack createQuestInstance(Random random) {
+			return new ItemStack(item, min == max ? min : random.nextInt(max - min) + min);
+		}
+	}
 }
